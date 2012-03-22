e8ef5f5b396966d3636b078993da10cb3d0b3c64
Dan Korostelev <nadako@gmail.com>
2012-03-22 12:09:31 +0400

    Rework game a bit: introduce game states, so we can have menus and stuff.

--- game.py
+++ game.py
@@ -22,64 +22,77 @@ from temp import get_wall_tex, floor_tex, dungeon_tex
 from generator import LayoutGenerator
 
 
+class GameState(object):
+
+    def __init__(self, game):
+        self.game = game
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def resume(self):
+        pass
+
 
 class Game(object):
 
-    ZOOM = 3
+    def __init__(self):
+        self.window = pyglet.window.Window(1024, 768, 'Dungeon')
+        self._states = []
+
+    def change_state(self, state):
+        if self._states:
+            prev_state = self._states.pop()
+            prev_state.exit()
+        self._states.append(state)
+        state.enter()
+
+    def push_state(self, state):
+        if self._states:
+            self._states[-1].pause()
+        self._states.append(state)
+        state.enter()
+
+    def pop_state(self):
+        if self._states:
+            prev_state = self._states.pop()
+            prev_state.exit()
+        if self._states:
+            self._states[-1].resume()
+
+    def run(self):
+        self.push_state(PlayLevelState(self))
+        pyglet.app.run()
+
+    def quit(self):
+        while self._states:
+            self.pop_state()
+        pyglet.app.exit()
+
+
+class PlayLevelState(GameState):
+
     DUNGEON_SIZE_X = 100
     DUNGEON_SIZE_Y = 100
+    ZOOM = 3
 
-    def __init__(self, window):
-        self.window = window
+    def enter(self):
         self._g_root = greenlet.getcurrent()
-        self._g_mainloop = greenlet.greenlet(self.gameloop)
-
-    def _render_level(self):
-        vertices = []
-        tex_coords = []
-
-        for x in xrange(self.level.size_x):
-            for y in xrange(self.level.size_y):
-                x1 = x * 8
-                x2 = x1 + 8
-                y1 = y * 8
-                y2 = y1 + 8
-
-                for entity in self.level.position_system.get_entities_at(x, y):
-                    renderable = entity.get(LayoutRenderable)
-                    if renderable:
-                        tile = renderable.tile
-                        break
-                else:
-                    continue
-
-                # always add floor, because we wanna draw walls above floor
-                vertices.extend((x1, y1, x2, y1, x2, y2, x1, y2))
-                tex_coords.extend(floor_tex.tex_coords)
-
-                if tile == LayoutGenerator.TILE_WALL:
-                    # if we got wall, draw it above floor
-                    tex = get_wall_tex(self.level.get_wall_transition(x, y))
-                    vertices.extend((x1, y1, x2, y1, x2, y2, x1, y2))
-                    tex_coords.extend(tex.tex_coords)
-
-        group = TextureGroup(dungeon_tex)
-        self._level_batch = pyglet.graphics.Batch()
-        self._level_vlist = self._level_batch.add(len(vertices) / 2, pyglet.gl.GL_QUADS, group,
-            ('v2i/static', vertices),
-            ('t3f/statc', tex_coords),
-        )
-
-    def gameloop(self):
-        self._text_overlay_batch = pyglet.graphics.Batch()
-
-        self._message_log = MessageLog()
-        self._last_messages_view = LastMessagesView(self._message_log, self.window.width, self.window.height)
+        self._g_loop = greenlet.greenlet(self._loop)
 
         self.level = Level(self, self.DUNGEON_SIZE_X, self.DUNGEON_SIZE_Y)
-
         self._render_level()
         self._light_overlay = LightOverlay(self.level.size_x, self.level.size_y)
+        self._text_overlay_batch = pyglet.graphics.Batch()
+        self._message_log = MessageLog()
+        self._last_messages_view = LastMessagesView(self._message_log, self.game.window.width, self.game.window.height)
+
         self._memento = {}
 
         room = random.choice(self.level._layout.rooms) # TODO: refactor this to stairs up/down
@@ -89,48 +102,43 @@ class Game(object):
         self.player.get(FOV).update_light()
 
         self._player_status = pyglet.text.Label(font_name='eight2empire', anchor_y='bottom')
-        self._camera = Camera(self.window, self.ZOOM, self.player)
+        self._camera = Camera(self.game.window, self.ZOOM, self.player)
 
-        while True:
-            self._update_player_status()
-            self.level.tick()
+        self.game.window.push_handlers(self)
 
-    def _update_player_status(self):
-        fighter = self.player.get(Fighter)
-        item_names = []
-        for item in self.player.get(Inventory).items:
-            name = get_name(item)
-            item_component = item.get(Item)
-            if item_component.quantity > 1:
-                name += ' (%d)' % item_component.quantity
-            item_names.append(name)
-        inventory = ', '.join(item_names) or 'nothing'
-        text = 'HP: %d/%d, ATK: %d, DEF: %d (INV: %s)' % (fighter.health, fighter.max_health, fighter.attack, fighter.defense, inventory)
-        self._player_status.text = text
+        self._g_loop.switch()
 
-    def _on_player_fov_update(self, old_lightmap, new_lightmap):
-        # update light overlay
-        self._light_overlay.update_light(new_lightmap, self._memento)
+    def exit(self):
+        self.game.window.remove_handlers(self)
 
-        # set in_fov flags
-        keys = set(old_lightmap).intersection(new_lightmap)
-        for key in keys:
-            for entity in self.level.position_system.get_entities_at(*key):
-                infov = entity.get(InFOV)
-                if infov:
-                    infov.in_fov = key in new_lightmap
+    def on_key_press(self, sym, mod):
+        command = None
 
-    def start(self):
-        self.window.push_handlers(self)
-        self._g_mainloop.switch()
+        if sym == key.NUM_8:
+            command = Command(Command.MOVE, (0, 1))
+        elif sym == key.NUM_2:
+            command = Command(Command.MOVE, (0, -1))
+        elif sym == key.NUM_4:
+            command = Command(Command.MOVE, (-1, 0))
+        elif sym == key.NUM_6:
+            command = Command(Command.MOVE, (1, 0))
+        elif sym == key.NUM_7:
+            command = Command(Command.MOVE, (-1, 1))
+        elif sym == key.NUM_9:
+            command = Command(Command.MOVE, (1, 1))
+        elif sym == key.NUM_1:
+            command = Command(Command.MOVE, (-1, -1))
+        elif sym == key.NUM_3:
+            command = Command(Command.MOVE, (1, -1))
+        elif sym == key.NUM_5:
+            command = Command(Command.WAIT, None)
+        elif sym == key.G:
+            command = Command(Command.PICKUP, None)
+        elif sym == key.D:
+            command = Command(Command.DROP, None)
 
-    def get_command(self):
-        return self._g_root.switch()
-
-    def message(self, text, color=(255, 255, 255, 255)):
-        if color:
-            text = '{color (%d, %d, %d, %d)}%s' % (color + (text,))
-        self._message_log.add_message(text)
+        if command is not None:
+            self._g_loop.switch(command)
 
     def on_draw(self):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
@@ -185,40 +193,83 @@ class Game(object):
                 sprite.draw()
                 gl.glPopMatrix()
 
-
     def _draw_hud(self):
         self._last_messages_view.draw()
         self._player_status.draw()
 
-    def on_key_press(self, sym, mod):
-        command = None
+    def _render_level(self):
+        vertices = []
+        tex_coords = []
 
-        if sym == key.NUM_8:
-            command = Command(Command.MOVE, (0, 1))
-        elif sym == key.NUM_2:
-            command = Command(Command.MOVE, (0, -1))
-        elif sym == key.NUM_4:
-            command = Command(Command.MOVE, (-1, 0))
-        elif sym == key.NUM_6:
-            command = Command(Command.MOVE, (1, 0))
-        elif sym == key.NUM_7:
-            command = Command(Command.MOVE, (-1, 1))
-        elif sym == key.NUM_9:
-            command = Command(Command.MOVE, (1, 1))
-        elif sym == key.NUM_1:
-            command = Command(Command.MOVE, (-1, -1))
-        elif sym == key.NUM_3:
-            command = Command(Command.MOVE, (1, -1))
-        elif sym == key.NUM_5:
-            command = Command(Command.WAIT, None)
-        elif sym == key.G:
-            command = Command(Command.PICKUP, None)
-        elif sym == key.D:
-            command = Command(Command.DROP, None)
+        for x in xrange(self.level.size_x):
+            for y in xrange(self.level.size_y):
+                x1 = x * 8
+                x2 = x1 + 8
+                y1 = y * 8
+                y2 = y1 + 8
 
-        if command is not None:
-            self._g_mainloop.switch(command)
+                for entity in self.level.position_system.get_entities_at(x, y):
+                    renderable = entity.get(LayoutRenderable)
+                    if renderable:
+                        tile = renderable.tile
+                        break
+                else:
+                    continue
 
+                # always add floor, because we wanna draw walls above floor
+                vertices.extend((x1, y1, x2, y1, x2, y2, x1, y2))
+                tex_coords.extend(floor_tex.tex_coords)
+
+                if tile == LayoutGenerator.TILE_WALL:
+                    # if we got wall, draw it above floor
+                    tex = get_wall_tex(self.level.get_wall_transition(x, y))
+                    vertices.extend((x1, y1, x2, y1, x2, y2, x1, y2))
+                    tex_coords.extend(tex.tex_coords)
+
+        group = TextureGroup(dungeon_tex)
+        self._level_batch = pyglet.graphics.Batch()
+        self._level_vlist = self._level_batch.add(len(vertices) / 2, pyglet.gl.GL_QUADS, group,
+            ('v2i/static', vertices),
+            ('t3f/statc', tex_coords),
+        )
+
+    def _update_player_status(self):
+        fighter = self.player.get(Fighter)
+        item_names = []
+        for item in self.player.get(Inventory).items:
+            name = get_name(item)
+            item_component = item.get(Item)
+            if item_component.quantity > 1:
+                name += ' (%d)' % item_component.quantity
+            item_names.append(name)
+        inventory = ', '.join(item_names) or 'nothing'
+        text = 'HP: %d/%d, ATK: %d, DEF: %d (INV: %s)' % (fighter.health, fighter.max_health, fighter.attack, fighter.defense, inventory)
+        self._player_status.text = text
+
+    def _on_player_fov_update(self, old_lightmap, new_lightmap):
+        # update light overlay
+        self._light_overlay.update_light(new_lightmap, self._memento)
+
+        # set in_fov flags
+        keys = set(old_lightmap).intersection(new_lightmap)
+        for key in keys:
+            for entity in self.level.position_system.get_entities_at(*key):
+                infov = entity.get(InFOV)
+                if infov:
+                    infov.in_fov = key in new_lightmap
+
+    def _loop(self):
+        while True:
+            self._update_player_status()
+            self.level.tick()
+
+    def get_command(self):
+        return self._g_root.switch()
+
+    def message(self, text, color=(255, 255, 255, 255)):
+        if color:
+            text = '{color (%d, %d, %d, %d)}%s' % (color + (text,))
+        self._message_log.add_message(text)
 
     def animate_damage(self, x, y, dmg):
         # hacky hack
@@ -226,8 +277,8 @@ class Game(object):
         start_y = (y * 8 + random.randint(0, 4)) * self.ZOOM
 
         label = pyglet.text.Label('-' + str(dmg), font_name='eight2empire', color=(255, 0, 0, 255),
-                                  x=x, y=start_y, anchor_x='center', anchor_y='bottom',
-                                  batch=self._text_overlay_batch)
+            x=x, y=start_y, anchor_x='center', anchor_y='bottom',
+            batch=self._text_overlay_batch)
 
         def update_label(animation):
             label.y = start_y + 12 * self.ZOOM * animation.anim_time
--- main.py
+++ main.py
@@ -3,9 +3,5 @@ pyglet.options['debug_gl'] = False
 
 from game import Game
 
-window = pyglet.window.Window(1024, 768, 'Dungeon')
-
-game = Game(window)
-game.start()
-
-pyglet.app.run()
+game = Game()
+game.run()
